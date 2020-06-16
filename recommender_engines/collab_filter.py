b18c899472c01eb7d4ad5c81c0612c2d6a5eae8c
PeterBrEsade <58176737+PeterBresade@users.noreply.github.com>
2020-06-16 13:05:02 +0200

    Update collab_filter.py

--- recommender_engines/collab_filter.py
+++ recommender_engines/collab_filter.py
@@ -62,3 +62,58 @@ def topMatches(data, person):
     scores = pd.DataFrame(scores, columns=['User', 'Avg_rating', 'R_score'])
     scores = scores.sort_values(by='R_score', ascending=False).reset_index(drop=True)
     return scores
+
+
+def getRecommendations(data,person,n=50,similarity=pearsonSimilarity):
+    """
+    Description: makes a dataframe with anime recommended for person
+    :param data: the anime dataframe, person to which recommend anime for 
+    and number of users person should be compared with
+    :return: dataframe with recommended anime
+    """
+    
+    #select the anime ranked by person
+    prefs1 = data.loc[data['User'] == person].dropna(axis='columns').drop(['User'],axis=1)
+    
+    
+    person_avg = 
+    n_nearest_neighbors = topMatches(data,person)[0:n]
+    ranking = n_nearest_neighbors['R_score']
+    rating = n_nearest_neighbors['Avg_rating']
+    
+    #only keep the rating of users within to top n similarity ranking
+    top =n_nearest_neighbors.merge(anime,on='User', how='left').drop(['User', 'R_score','Avg_rating'],axis=1).dropna(axis=1, how='all')
+    
+    
+    #itterate over all anime which are ranked by similar user
+    for column in top:
+        Sums=0
+        tot_rank=0
+        tot=0
+        #check if anime hasn't been seen by person 
+        #and check whether there are enough ranking to be sure about recommendation
+        #I chose treshold to be 5
+        if column in prefs1 or top[column].count() < 6:
+            continue
+        #itterate over all rows and calculate similarity function for anime
+        for i in top[column].iteritems():
+            if pd.notna(i[1]):
+                rank = ranking.iloc[i[0]]
+                tot_rank =rank+tot_rank
+                user_avg =rating.iloc[i[0]]
+                sums = (i[1]-user_avg)*rank
+                Sums = sums + Sums
+                tot = person_avg + Sums/tot_rank
+            #make sure anime are not ranked above 5 or below 0
+            if tot >5:
+                totals = pd.DataFrame({'anime': [column], 'recom': [5]})
+            elif tot <0:
+                totals = pd.DataFrame({'anime': [column], 'recom': [0]})
+            else:
+                totals = pd.DataFrame({'anime': [column], 'recom': [tot]})
+            
+        total= pd.concat([total,totals], ignore_index=True)
+
+    total = total.sort_values(by = 'recom' ,ascending=False).reset_index(drop=True)
+
+    return total
